26352768f6f32ced9122cf99c326174a7442ba9e
dangtrinh1811 <dangtrinh1811@gmail.com>
2024-06-30 11:07:32 +0700

    add file for more player animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -86,11 +86,6 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    public void OnLanding()
-    {
-        animator.SetBool("isJumping", false);
-    }
-
     private void FixedUpdate()
     {
         //rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
